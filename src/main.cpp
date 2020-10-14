d696a2c36e10a5922c50341d3337c495e59a3be2
reapr <virkshan@gmail.com>
2020-10-14 10:49:11 -0400

    🐛 fix(main): Updated to SDL2 and created window

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
-#include <SDL.h>
+#include <SDL2/SDL.h>
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
-    }
-    else
-    {
-        cout << "SDL initialization succeeded!";
+        cout << "Failed to initialize the SDL2 library\n";
+        return -1;
     }
 
-    cin.get();
-    return 0;
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if (!window)
+    {
+        cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if (!window_surface)
+    {
+        cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
 }
