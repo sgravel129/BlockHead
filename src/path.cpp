1be2117faf66e802b14a6008b01f86c90175ac93
Sebastien Gravel <15smg7@queensu.ca>
2020-10-28 21:20:45 -0400

    🚧 wip(path): Started zombie pathfinding

---
+++ src/path.cpp
@@ -0,0 +1,28 @@
+#include "path.hpp"
+#include "constants.hpp"
+#include <vector>
+
+
+void abstractMap() {
+    std::vector<Node> entranceS;
+}
+/*
+std::vector<std::vector<Node>> buildClusters(int width, int height) {
+    int clusterNum = (X_MAX/width) * (Y_MAX/height);
+
+}
+*/
+
+void buildGraph() {
+
+}
+
+void addLevelToGraph(int l) {
+
+}
+
+void preprocessing(int maxLevel) {
+    abstractMap();
+    buildGraph();
+
+}
