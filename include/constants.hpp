b9d5ebb2ce05057135d6c8faee91bbaf3d19e5a2
Shan Virk <15sv26@caslab.queensu.ca>
2020-11-26 11:28:20 -0500

    🐛 fix(constants): Removed multiple defintions

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-const char *GAME_NAME = "BlockHead";
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+static const char *GAME_NAME = "BlockHead";
+static const int SCREEN_WIDTH = 1280;
+static const int SCREEN_HEIGHT = 720;
