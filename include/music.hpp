9344dc89686d8481d0145348af1b625806975430
Shan Virk <15sv26@caslab.queensu.ca>
2020-11-30 14:00:40 -0500

    🔨 tools: Mixer Support for linux/OSX

--- include/music.hpp
+++ include/music.hpp
@@ -1,9 +1,17 @@
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
+#pragma once
+
+#if defined(_WIN32)
+	#include <SDL.h>
+    #include <SDL_image.h>
+    #include <SDL_mixer.h>
+#else
+	#include <SDL2/SDL.h>
+    #include <SDL2/SDL_image.h>
+    #include <SDL2/SDL_mixer.h>
+#endif
+
 #include <stdio.h>
 #include <string>
-
 #include "log.hpp"
 
 bool initMusic();
