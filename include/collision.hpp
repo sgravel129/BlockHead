234f1b490d1369b01e6e035865abf6ed801e89ec
wesvantil <15wmv@queensu.ca>
2020-11-30 18:10:37 -0500

    map and sprite update

--- include/collision.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-class collision
-{
-public:
-	static bool AABB(const SDL_Rect& recA, const SDL_Rect& recB);
-	static bool AABB(const hasCollision& colA, const hasCollision& colb)
-};
